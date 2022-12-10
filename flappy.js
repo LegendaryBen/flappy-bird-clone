2181bd4a4b83d7b3afa1032a490f4601a367acdd
Chukwuezi Benjamin Chimaobi <benjaminchima072@gmail.com>
2022-12-10 01:44:12 +0000

    i completed the the top obstacles for the game

--- flappy.js
+++ flappy.js
@@ -6,9 +6,11 @@ canvas.height = window.innerHeight;
 
 let bottomObstaclesArray = [];
 
+let topObstaclesArray = [];
+
 let colors = ["red","blue","green","yellow"];
 
-let heights = [(canvas.height/2)-10,(canvas.height/2)-40,(canvas.height/2)-70,(canvas.height/2)-100,(canvas.height/2)-130];
+let heights = [(canvas.height/2)-70,(canvas.height/2)-90,(canvas.height/2)-120,(canvas.height/2)-150,(canvas.height/2)-200];
 
 function Obstacle(){
     this.number1 = Math.floor(Math.random()*colors.length);
@@ -21,10 +23,26 @@ function Obstacle(){
     
 }
 
+function Obstacle2(){
+    this.number1 = Math.floor(Math.random()*colors.length);
+    this.number2 =  Math.floor(Math.random()*heights.length);
+    this.color = colors[this.number1];
+    this.width = 50;
+    this.height = heights[this.number2];
+    this.x = window.innerWidth;
+    this.y = 0;
+}
+
 
 function fillBottomObstacle(){
     let obs = new Obstacle();
     bottomObstaclesArray.push(obs);
+}
+
+function fillTopObstacle(){
+    let obs = new Obstacle2();
+    topObstaclesArray.push(obs);
+
 }
 
 function drawBottomObstacle(){
@@ -35,6 +53,15 @@ function drawBottomObstacle(){
     }
 }
 
+function drawTopObstacle(){
+    for(let i = 0;i<topObstaclesArray.length;i++){
+        let obs = topObstaclesArray[i];
+        ctx.fillStyle = obs.color;
+        ctx.fillRect(obs.x,obs.y,obs.width,obs.height);
+    }
+}
+
+
 function updateBottomObstacle(){
     let controller = -5;
     for(let i = 0;i<bottomObstaclesArray.length;i++){
@@ -43,28 +70,51 @@ function updateBottomObstacle(){
     }
 }
 
+function updateTopObstacle(){
+    let controller = -5;
+    for(let i = 0;i<topObstaclesArray.length;i++){
+        let obs = topObstaclesArray[i];
+        obs.x += controller;
+    }
+}
+
+
+
 function controlObstacleArray(){
     for(let i = 0;i<bottomObstaclesArray.length;i++){
         let obs =  bottomObstaclesArray[i];
         if(obs.x < -obs.width){
             bottomObstaclesArray.splice(i,1);
-            console.log(bottomObstaclesArray.length);
         }
     }
 }
 
+function controlObstacleArray2(){
+    for(let i = 0;i<topObstaclesArray.length;i++){
+        let obs =  topObstaclesArray[i];
+        if(obs.x < -obs.width){
+            topObstaclesArray.splice(i,1);
+        }
+    }
+}
+
+
+
 function gameLoop(){
     ctx.clearRect(0,0,window.innerWidth,window.innerHeight);
     drawBottomObstacle();
+    drawTopObstacle();
     updateBottomObstacle();
+    updateTopObstacle();
     controlObstacleArray();
+    controlObstacleArray2();
     requestAnimationFrame(gameLoop);
 }
 
 gameLoop();
 
-setInterval(fillBottomObstacle,1500);
-
+setInterval(fillBottomObstacle,1200);
+setInterval(fillTopObstacle,1700);
 
 
 
